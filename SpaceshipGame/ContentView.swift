5e39738b6115c496faad407247e1a1524fcf0aae
Bulat Kamalov <kamalovmarket@gmail.com>
2023-07-28 13:25:42 +0400

    Initial Commit

---
+++ SpaceshipGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceshipGame
+//
+//  Created by Bulat Kamalov on 28.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceshipGame/SpaceshipGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceshipGameApp.swift
+//  SpaceshipGame
+//
+//  Created by Bulat Kamalov on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceshipGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
